5d57eb71be21f3f0a02ee62864f37e1e099daf36
dolphin-pc <pcx474@gmail.com>
2024-06-13 14:54:09 +0900

    Squashed commit of the following:
    commit f790cc56889e15dfea9d6ca7dafb66a1db861628
    Author: dolphin-pc <pcx474@gmail.com>
    Date:   Thu Jun 13 14:53:09 2024 +0900
    
        [MODIFY] 글작성화면, image 크기 조정
    
    commit e10da54fcb71233482cf0cfb7c7071c8205ff881
    Merge: 22f7e77 24a9b27
    Author: dolphin-pc <pcx474@gmail.com>
    Date:   Thu Jun 13 14:36:35 2024 +0900
    
        Merge branch 'dev' into 박찬영_브랜치
    
    commit 22f7e77ccb3bdad6bb78a34ede2e44ecedfab923
    Author: dolphin-pc <pcx474@gmail.com>
    Date:   Thu Jun 13 14:36:18 2024 +0900
    
        [ADD] editor 적용
    
    commit 24a9b27d64a61fbf9d56354e53f07e343e7c3b48
    Author: kim-museong <antjd0419@gmail.com>
    Date:   Thu Jun 13 14:29:03 2024 +0900
    
        kms merge
    
    commit e095390855b9be8e8d813ce5dcb8e231e8a7cb7d
    Merge: 4256d88 693c4cb
    Author: kim-museong <antjd0419@gmail.com>
    Date:   Thu Jun 13 14:25:16 2024 +0900
    
        dev merge
    
    commit 4256d88bc0f6d92401596f2cf4186b01030ad047
    Author: kim-museong <antjd0419@gmail.com>
    Date:   Thu Jun 13 13:57:07 2024 +0900
    
        date별 정렬기능추가
    
    commit 693c4cb4365e246f9e47d4f530ff644c7f66ba82
    Merge: 874f6da 49cfb5d
    Author: je-pa <jieun0502@gmail.com>
    Date:   Thu Jun 13 13:46:51 2024 +0900
    
        Merge branch '박지은_브랜치' of https://github.com/DevMinds-inno/DevMinds into dev
    
    commit 49cfb5d430442883f2e4c8f566d4269d62991398
    Merge: 7f80492 e355478
    Author: je-pa <jieun0502@gmail.com>
    Date:   Thu Jun 13 13:38:10 2024 +0900
    
        [ADD] board 상세보기 이미지 추가
    
    commit 874f6da85f8ca5a669baa986d11826749f29c0f7
    Author: dolphin-pc <pcx474@gmail.com>
    Date:   Thu Jun 13 11:37:41 2024 +0900
    
        [ADD] 글 삭제 기능 추가
    
    commit 7f80492ecf6a3f9f2cd6f177411e4abd5093c4b0
    Author: je-pa <jieun0502@gmail.com>
    Date:   Thu Jun 13 09:37:21 2024 +0900
    
        [ADD] board 디테일
    
    commit 01bc53686fb359d96d1ebb3165a1b8282768286d
    Author: kim-museong <antjd0419@gmail.com>
    Date:   Thu Jun 13 09:11:41 2024 +0900
    
        commit

--- static/main/main.js
+++ static/main/main.js
@@ -3,23 +3,36 @@ const cardList = document.getElementById("card-list");
 const sortLike = document.getElementById("like");
 const sortRecent = document.getElementById("recent");
 const underLine = document.querySelector(".sort-bar-underline");
-let sortType = "recent"; //기본정렬값
-let page = 1; // 기본페이지
+const selectButton = document.querySelector(".home-tag-right-selecter");
+const selectValue = document.querySelector(".home-tag-right-value");
+const selectBar = document.querySelector(".home-tag-right-selecter-bar");
+const selectBarList = document.querySelector(".home-tag-right-list");
+const PER_PAGE = 8; // 한번에 가져올 페이지개수
 
-// 로딩 더미 생성 함수
+// 변수공간
+let sortType = "";
+let sortDate = "";
+let page = 1; // 기본페이지
+let hasMoreData = true; //데이터를 더 가져올 수 있는지 여부
+let isLoading = false;
+let isVisible = false; // 셀렉트창
+
+// 더미 생성 함수
 const createLoadingDummy = () => {
-  Array.from({ length: 20 }).forEach(() => {
+  Array.from({ length: PER_PAGE }).forEach(() => {
     const postElement = document.createElement("li");
     postElement.classList.add("card");
     postElement.innerHTML = `
-            <a href="">
+            <span>
               <div class="card-image-box dummy">
                 <img class="dummy-card-image"/>
               </div>
-            </a>
+            </ㅔ>
             <div class="card-main">
               <div class="card-main-title dummy dummy-text"></div>
-              <div class="card-main-content dummy"></div>
+              <div class="card-main-content dummy">
+                <p></p>
+              </div>
               <div class="card-sub-info dummy dummy-text">
                 <span class="card-date dummy dummy-text"></span>
                 <span class="dummy dummy-text"></span>
@@ -37,98 +50,187 @@ const createLoadingDummy = () => {
   });
 };
 
-// // 로딩 더미 초기 생성
-// createLoadingDummy();
+const createBoardList = (data) => {
+  data.forEach((board) => {
+    const postElement = document.createElement("li");
+    postElement.classList.add("card");
+    postElement.innerHTML = `
+        ${
+          board.img_src
+            ? `
+          <a href="/boards/${board.id}">
+            <div class="card-image-box">
+              <img class="card-image" src="${board.img_src}" alt="${board.title}_image" />
+            </div>
+          </a>`
+            : ""
+        }
+     
+        <div class="card-main">
+          <a href="/boards/${board.id}">
+            <h4 class="card-main-title">${board.title}</h4>
+            <div class="card-main-content">
+              <p>${board.content}</p>
+            </div>
+          </a>
+          <div class="card-sub-info">
+            <span class="card-date">${board.updated_dttm}</span>
+            <span>·</span>
+            <span>0개의 댓글</span>
+          </div>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-user">
+            by <span>${board.writer}</span>
+          </div>
+          <div class="card-footer-like"><i class="bi bi-heart-fill"></i>0</div>
+        </div>
+        `;
+    cardList.appendChild(postElement);
+  });
+};
 
-/* posts가져오기함수 */
-const loadPosts = async () => {
-  //   loadingdummy.style.display = "block"; // 로딩화면 보이기
+// 더미 숨기는 함수
+const hideDummy = () => {
+  loadingdummy.style.display = "none";
+};
+
+const resetCardList = () => {
+  cardList.innerHTML = "";
+  page = 1;
+  hasMoreData = true;
+};
+
+// boards 요청 함수
+const loadboards = async () => {
+  // 로딩중이거나 가져올 데이터가 없을 때
+  if (isLoading || !hasMoreData) return;
+
+  isLoading = true;
+
+  if (isLoading) {
+    loadingdummy.style.display = "grid";
+  }
 
   try {
-    const perPage = 20; // 한번에 가져올 페이지개수
     const response = await fetch(
-      `/posts?sortType=${sortType}&page=${page}&per_page=${perPage}`
+      `/api/boards/${sortType || "recent"}/${
+        sortDate || "week"
+      }?&page=${page}&per_page=${PER_PAGE}`
     );
+
     if (!response.ok) {
       throw new Error("요청 오류");
     }
+
     const data = await response.json();
-    console.log(data);
 
-    // if (!data) {
-    //   cardList.innerHTML = "";
-    //   const postElement = document.createElement("div");
-    //   postElement.textContent = "게시물이 없습니다.";
-    //   cardList.appendChild(postElement);
-    //   return;
-    // }
+    // 보드가 없을 때
+    if (data.length === 0) {
+      hasMoreData = false;
+      hideDummy();
+      console.log("포스트 없습니다.");
+      return;
+    }
 
-    data.forEach((post) => {
-      const postElement = document.createElement("li");
-      postElement.classList.add("card");
-      postElement.innerHTML = `
-          <a href="">
-            <div class="card-image-box">
-              <img class="card-image" src="" alt="" />
-            </div>
-          </a>
-          <div class="card-main">
-          <div>${post.id}</div>
-            <div class="card-main-title">${post.title}</div>
-            <div class="card-main-content">${post.content}</div>
-            <div class="card-sub-info">
-              <span class="card-date">${post.updated_dttm}</span>
-              <span>·</span>
-              <span>댓글수</span>
-            </div>
-          </div>
-          <div class="card-footer">
-            <div class="card-footer-user">
-              by <span>${post.writer}</span>
-            </div>
-            <div class="card-footer-like">좋아요수: 0</div>
-          </div>
-          `;
-      cardList.appendChild(postElement);
-    });
+    createBoardList(data);
     page++; // 페이지 증가
+
+    // 가져온 데이터의 개수가 PER_PAGE보다 작으면 로딩 화면을 숨김
+    if (data.length < PER_PAGE) {
+      hideDummy();
+      hasMoreData = false;
+    }
   } catch (e) {
     console.log("에러발생", e);
   } finally {
-    loadingdummy.style.display = "none"; // 로딩화면 숨기기
+    hideDummy();
+    isLoading = false;
   }
 };
 
 const handleScroll = () => {
+  if (isLoading) return;
+
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-    console.log("추가 포스트를 불러오는중...");
-    loadPosts();
+    loadboards();
   }
 };
 
 window.addEventListener("scroll", handleScroll);
-
-// 초기 데이터 불러오기
-setTimeout(() => loadPosts(true), 1000);
-
-// 좋아요 정렬 버튼 클릭 시
-sortLike.addEventListener("click", function () {
-  underLine.style.left = "3%";
-  this.classList.add("sort-active");
-  sortRecent.classList.remove("sort-active");
-  sortType = "like";
-  cardList.innerHTML = ""; // 리스트 초기화
-  page = 1; // 기본페이지
-  loadPosts();
+document.addEventListener("DOMContentLoaded", () => {
+  createLoadingDummy();
+  loadboards();
 });
 
-// 최신순 정렬 버튼 클릭 시
-sortRecent.addEventListener("click", function () {
-  underLine.style.left = "44%";
-  sortType = "recent";
-  this.classList.add("sort-active");
-  sortLike.classList.remove("sort-active");
-  cardList.innerHTML = ""; // 리스트 초기화
-  page = 1; // 기본페이지
-  loadPosts();
+// home-tag-left
+sortLike.addEventListener("click", () => changeSortType("like"));
+sortRecent.addEventListener("click", () => changeSortType("recent"));
+
+const changeSortType = (type) => {
+  if (type === "recent") {
+    underLine.style.left = "44%";
+    sortLike.classList.remove("sort-active");
+    sortRecent.classList.add("sort-active");
+  }
+
+  if (type === "like") {
+    underLine.style.left = "3%";
+    sortRecent.classList.remove("sort-active");
+    sortLike.classList.add("sort-active");
+  }
+
+  sortType = type;
+  history.pushState(null, null, `/${sortType}`);
+  resetCardList();
+  loadboards();
+};
+
+// home-tag-right
+const toggleSelectBar = () => {
+  if (isVisible) {
+    hideSelectBar();
+  } else {
+    showSelectBar();
+  }
+  isVisible = !isVisible;
+};
+
+const showSelectBar = () => {
+  selectBar.classList.add("visible");
+};
+
+const hideSelectBar = () => {
+  selectBar.classList.remove("visible");
+};
+
+// 버튼 클릭 시 토글
+selectButton.addEventListener("click", (e) => {
+  e.stopPropagation(); // 이벤트 버블링 방지
+  toggleSelectBar();
+});
+
+// 다른 영역 클릭 시에도 숨기기
+document.addEventListener("click", (e) => {
+  if (isVisible && !selectBar.contains(e.target) && e.target !== selectButton) {
+    hideSelectBar();
+    isVisible = false;
+  }
+});
+
+selectBarList.addEventListener("click", (e) => {
+  const childrenArray = selectBarList.children;
+  const { id, textContent } = e.target;
+  selectValue.textContent = textContent;
+
+  [...childrenArray].forEach((element) => {
+    element.classList.remove("selected");
+  });
+
+  e.target.classList.add("selected");
+
+  sortDate = id;
+  history.pushState(null, null, `/${sortType}/${sortDate}`);
+  resetCardList();
+  loadboards();
 });
--- static/write/write.js
+++ static/write/write.js
@@ -32,23 +32,19 @@ class Write {
     }
   }
 
-  back() {
-    history.back();
-    history.replaceState(null, null, "/");
-  }
-
   out(inputElements) {
     let isNotEmpty = inputElements.some(($el) => $el.val().trim() !== "");
 
-    if (!isNotEmpty) return this.back();
+    if (!isNotEmpty) return Util.back();
 
     if (confirm("입력한 내용이 있습니다. 정말로 나가시겠습니까?")) {
-      this.back();
+      Util.back();
     }
   }
 
-  async newSave(formThis) {
-    const reqData = write.getFormData($(formThis));
+  async newSave($form, addData = {}) {
+    const reqData = Write.getFormData($($form));
+    Object.assign(reqData, addData);
 
     const res = await fetch("/api/boards", {
       method: "POST",
@@ -70,10 +66,11 @@ class Write {
       : (location.href = "/");
   }
 
-  async editSave(formThis) {
+  async editSave($form, addData = {}) {
     const writeObj = this;
 
-    const reqData = write.getFormData($(formThis));
+    const reqData = Write.getFormData($($form));
+    Object.assign(reqData, addData);
 
     if (!writeObj.formData) throw new Error("formData is not defined");
 
@@ -98,7 +95,7 @@ class Write {
       : (location.href = "/");
   }
 
-  getFormData($form) {
+  static getFormData($form) {
     return $form.serializeArray().reduce((acc, input) => {
       acc[input.name] = input.value;
       return acc;
@@ -113,36 +110,60 @@ class Write {
   }
 }
 
+class ToastEditor {
+  constructor(inputId, initContent) {
+    this.editor = new toastui.Editor({
+      el: document.querySelector(inputId), // 에디터를 적용할 요소 (컨테이너)
+      height: "100%", // 에디터 영역의 높이 값 (OOOpx || auto)
+      initialEditType: "markdown", // 최초로 보여줄 에디터 타입 (markdown || wysiwyg)
+      initialValue: initContent, // 에디터의 초기 값
+      previewStyle: "vertical", // 'tab' or 'vertical'
+    });
+  }
+
+  initData(data) {
+    this.editor.setHTML(data);
+  }
+
+  setPreview($output) {
+    this.editor.on("change", (e) => {
+      const editorContent = this.editor.getHTML();
+      $output.html(editorContent);
+    });
+  }
+
+  getHTML() {
+    return this.editor.getHTML();
+  }
+}
+
 const write = new Write(data);
+
 $(document).ready(() => {
+  const $form = $("#write-form");
+  const $submitBtn = $("#submit-btn");
+
+  $submitBtn.on("click", (e) => {
+    write.newSave($form, { content: editor.getHTML() });
+  });
+
+  const editor = new ToastEditor("#content", data ? data.content : "");
+
   if (data) {
     // 편집모드
     const inputPassword = prompt("비밀번호를 입력해주세요.");
     if (inputPassword !== data.password) {
       alert("비밀번호가 일치하지 않습니다.");
-      write.back();
+      Util.back();
     }
 
-    $("#write-form").on("submit", (e) => {
-      e.preventDefault();
-      write.editSave(e.target);
-    });
-    $("#writer").attr("readonly", "readonly");
-    $("#submit").text("수정하기");
-
-    for (const [key, value] of Object.entries(data)) {
-      write.bindValue(key, value);
-    }
-  } else {
-    // 작성모드
-    $("#write-form").on("submit", (e) => {
-      e.preventDefault();
-      write.newSave(e.target);
+    $submitBtn.off().on("click", (e) => {
+      write.editSave($form, { content: editor.getHTML() });
     });
   }
 
   write.onInput($("#title"), $("#result-title"));
-  write.onInput($("#content"), $("#result-content"));
+  editor.setPreview($("#result-content"));
   write.onInput($("#img_src"), $("#result-image"));
 
   $("#back-btn").on("click", () => write.out([$("#title"), $("#content")]));
