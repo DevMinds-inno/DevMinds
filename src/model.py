1ab6e672cf3d93f929077233b70b1d61520d14ac
dolphin-pc <pcx474@gmail.com>
2024-06-13 09:19:26 +0900

    [ADD] Boards 모델, img_src 속성 추가

--- src/model.py
+++ src/model.py
@@ -10,5 +10,6 @@ class Board(db.Model):
     content = db.Column(db.String(10000), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     writer = db.Column(db.String(20), nullable=False)
+    img_src = db.Column(db.String(1000), nullable=True)
     created_dttm = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_dttm = db.Column(db.DateTime, default=datetime.now, nullable=False)
